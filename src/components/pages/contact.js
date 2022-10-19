b355cabcdc4d131c7548a3271ae1580c17423b26
ErikGorin <ermuratiitep@gmail.com>
2022-10-19 21:49:09 +0300

    Initial commit

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -1,6 +1,27 @@
 import React from "react";
+import contactPagePicture from "../../../static/assets/images/tudor-baciu-vc3iVL_znJ8-unsplash.jpg";
 
 export default function(){
-    return <div>Contact</div>;
+    return (<div
+        className="content-page-wrapper">
+            <div className="left-column"
+                 style={{
+                    background: "url(" + contactPagePicture + ") no-repeat" , 
+                    backgroundSize: "cover",
+                    backgroundPosition: "center"
+                 }}
+                 ></div>
+            <div className="right-column">
+                <div className="contact-bullet-poits">
+                    <div className="bullet-point-group">
+                        <div className="icon">
+                            {'>'}
+                        </div>
+                        <div className="text">
+                            555-555-555
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>);
 }
-    
