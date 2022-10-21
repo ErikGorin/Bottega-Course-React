587c971b405c1277361445fb5f393e8056a3c201
ErikGorin <ermuratiitep@gmail.com>
2022-10-21 21:40:47 +0300

    Initial commit

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -22,52 +22,85 @@ class Blog extends Component {
     window.addEventListener("scroll", this.onScroll, false);
     this.handleNewBlogClick = this.handleNewBlogClick.bind(this);
     this.handleModalClose = this.handleModalClose.bind(this);
-    this.handleSuccessfulNewBlogSubmission = this.handleSuccessfulNewBlogSubmission.bind(this);
+    this.handleSuccessfulNewBlogSubmission = this.handleSuccessfulNewBlogSubmission.bind(
+      this
+    );
+    this.handleDeleteClick = this.handleDeleteClick.bind(this);
   }
 
-  handleSuccessfulNewBlogSubmission(blog){
-     this.setState({
-       blogModalIsOpen: false, 
-       blogItems: [blog].concat(this.state.blogItems)
-     })
+  handleDeleteClick(blog) {
+    axios
+      .delete(
+        `https://api.devcamp.space/portfolio/portfolio_blogs/${blog.id}`,
+        { withCredentials: true }
+      )
+      .then(response => {
+        this.setState({
+          blogItems: this.state.blogItems.filter(blogItem => {
+            return blog.id !== blogItem.id;
+          })
+        });
+
+        return response.data;
+      })
+      .catch(error => {
+        console.log("delete blog error", error);
+      });
   }
 
-  handleModalClose(){
+  handleSuccessfulNewBlogSubmission(blog) {
     this.setState({
-      blogModalIsOpen: false 
-    })
+      blogModalIsOpen: false,
+      blogItems: [blog].concat(this.state.blogItems)
+    });
   }
 
-  handleNewBlogClick(){
+  handleModalClose() {
+    this.setState({
+      blogModalIsOpen: false
+    });
+  }
+
+  handleNewBlogClick() {
     this.setState({
       blogModalIsOpen: true
-    })
+    });
   }
 
   onScroll() {
-      if(this.state.isLoading || this.state.blogItems.length === this.state.totalCount){
-        return;
-      }
+    if (
+      this.state.isLoading ||
+      this.state.blogItems.length === this.state.totalCount
+    ) {
+      return;
+    }
 
-      if (window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight)
-      {
-        this.getBlogItems();
-      }
+    if (
+      window.innerHeight + document.documentElement.scrollTop ===
+      document.documentElement.offsetHeight
+    ) {
+      this.getBlogItems();
+    }
   }
 
   getBlogItems() {
     this.setState({
       currentPage: this.state.currentPage + 1
     });
+
     axios
-      .get(`https://karl.devcamp.space/portfolio/portfolio_blogs?page=${this.state.currentPage}`, {
-        withCredentials: true
-      })
+      .get(
+        `https://karl.devcamp.space/portfolio/portfolio_blogs?page=${
+          this.state.currentPage
+        }`,
+        {
+          withCredentials: true
+        }
+      )
       .then(response => {
         console.log("gettting", response.data);
         this.setState({
-          blogItems: this.state.blogItems.concat(
-          response.data.portfolio_blogs), 
+          blogItems: this.state.blogItems.concat(response.data.portfolio_blogs),
           totalCount: response.data.meta.total_records,
           isLoading: false
         });
@@ -81,40 +114,54 @@ class Blog extends Component {
     this.getBlogItems();
   }
 
-  componentWillUnmount(){
-     window.removeEventListener("scroll", this.onScroll, false); 
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.onScroll, false);
   }
 
   render() {
     const blogRecords = this.state.blogItems.map(blogItem => {
-      return <BlogItem key={blogItem.id} blogItem={blogItem} />;
+      if (this.props.loggedInStatus === "LOGGED_IN") {
+        return (
+          <div key={blogItem.id} className="admin-blog-wrapper">
+            <BlogItem blogItem={blogItem} />
+            <a onClick={() => this.handleDeleteClick(blogItem)}>
+              <FontAwesomeIcon icon="trash" />
+            </a>
+          </div>
+        );
+      } else {
+        return <BlogItem key={blogItem.id} blogItem={blogItem} />;
+      }
     });
 
     return (
-              <div className="blog-container">
-                <BlogModal 
-                  handleSuccessfulNewBlogSubmission={this.handleSuccessfulNewBlogSubmission}
-                  handleModalClose ={this.handleModalClose}
-                  modalIsOpen = {this.state.blogModalIsOpen}/>
+      <div className="blog-container">
+        <BlogModal
+          handleSuccessfulNewBlogSubmission={
+            this.handleSuccessfulNewBlogSubmission
+          }
+          handleModalClose={this.handleModalClose}
+          modalIsOpen={this.state.blogModalIsOpen}
+        />
 
-                  {this.props.loggedInStatus === "LOGGED_IN"? (
-                    <div className="new-blog-link">
-                        <a onClick={this.handleNewBlogClick}>
-                          <FontAwesomeIcon icon="plus-circle"/>
-                        </a>
-                    </div> ) : null}
+        {this.props.loggedInStatus === "LOGGED_IN" ? (
+          <div className="new-blog-link">
+            <a onClick={this.handleNewBlogClick}>
+              <FontAwesomeIcon icon="plus-circle" />
+            </a>
+          </div>
+        ) : null}
 
-                <div className="content-container">{blogRecords}</div>
+        <div className="content-container">{blogRecords}</div>
 
-                {this.state.isLoading ?(
-                <div className="content-loader">
-                  <FontAwesomeIcon icon="spinner" spin />
-                </div>): null}
-                
-                </div>
-        );
-    
+        {this.state.isLoading ? (
+          <div className="content-loader">
+            <FontAwesomeIcon icon="spinner" spin />
+          </div>
+        ) : null}
+      </div>
+    );
   }
 }
- 
+
 export default Blog;
